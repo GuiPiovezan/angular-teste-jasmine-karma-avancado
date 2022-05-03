ffabb976f34fbd749564ddcb325d3fae85cc8a69
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2022-05-03 10:56:52 -0300

    Criando cenários de testes onde testamos a função like do componente

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -1,5 +1,10 @@
 import { PhotoFrameModule } from './photo-frame.module';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { PhotoFrameComponent } from './photo-frame.component';
 describe(PhotoFrameComponent.name, () => {
   let fixture: ComponentFixture<PhotoFrameComponent> = null;
@@ -16,5 +21,48 @@ describe(PhotoFrameComponent.name, () => {
 
   it('Should create component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+  should trigger (@Output liked) once when called multiples times within debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+
+    let times = 0;
+    component.liked.subscribe(() => times++);
+
+    component.like();
+    component.like();
+
+    tick(500);
+
+    expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+  should trigger (@Output liked) two times when called outside debounce time`, fakeAsync(() => {
+    fixture.detectChanges();
+
+    let times = 0;
+    component.liked.subscribe(() => times++);
+
+    component.like();
+    tick(500);
+
+    component.like();
+    tick(500);
+
+    expect(times).toBe(2);
+  }));
+
+  it(`Should display number of likes when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+
+    component.likes++;
+    fixture.detectChanges();
+
+    const element: HTMLElement =
+      fixture.nativeElement.querySelector('.like-counter');
+
+    expect(element.textContent.trim()).toBe('1');
   });
 });
